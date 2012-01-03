ada9cb64af42c19c0cdb4c54b3d9d4b98abcf10a
Arno Teigseth <arnotixe@gmail.com>
2012-01-03 09:43:10 -0500

    Added my first morph()-based rules, and builtin VERY basic grammar-tagged hunspell dictionary

--- pythonpath/lightproof_impl_en_US.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: UTF-8 -*-
-pkg = "en_US"
-lang = "en_US"
-locales = {'en-US': ['en', 'US', '']}
-version = "0.1"
-author = ""
--- pythonpath/lightproof_opts_en_US.py
+++
@@ -1,4 +0,0 @@
-lopts = {}
-lopts_default = {}
-lopts['en_US'] = [u'grammar', u'cap', u'dup', u'pair', u'spaces', u'mdash', u'quotation', u'times', u'spaces2', u'ndash', u'apostrophe', u'ellipsis', u'spaces3', u'minus', u'metric', u'numsep', u'nonmetric', u'falsefriends_en_UK']
-lopts_default['en_US'] = [u'spaces', u'times']
